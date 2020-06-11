27ff8a37e558aba65d5b11db854efe8681ee0150
jorge paredes <jalbert400@gmail.com>
2020-06-11 18:03:19 -0500

    se ha creado los ORM de usuarios, roles, permisos basico para el administrador

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,31 +6,39 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->bigInteger('role_id')->unsigned();
+      $table->string('nombre', 100);
+      $table->string('apellidos', 100);
+      $table->string('dni', 8);
+      $table->string('telefono', 9);
+      $table->string('email')->unique();
+      $table->string('username')->unique();
+      $table->string('password');
+      $table->char('estado', 1);
+      $table->rememberToken();
+      $table->text('session_id')->nullable()->default(null)->comment('Almacena el id de la sesión del usuario');
+      $table->timestamps();
+      $table->softDeletes();
+      $table->foreign('role_id')->references('id')->on('roles');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
